d238b6159cdf764e744862141de72f8df08df624
Evgeny Gunko <evgeny.gunko@gmail.com>
2025-11-15 17:35:43 +0000

    Merged PR 669: Use global XML namespaces
    Add "CopyWords.MAUI.Views" to global XML namespace

---
+++ source/CopyWords.MAUI/GlobalXmlns.cs
@@ -0,0 +1,11 @@
+﻿[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CopyWords.MAUI.Views")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CopyWords.MAUI.Converters")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CopyWords.MAUI.Helpers")]
+[assembly: XmlnsDefinition( "http://schemas.microsoft.com/dotnet/maui/global", "CopyWords.Core.ViewModels", AssemblyName = "CopyWords.Core")]
+
+// Syncfusion namespaces
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.Buttons", AssemblyName = "Syncfusion.Maui.Toolkit")]
+
+//[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunityToolkit.Maui.Converters", AssemblyName = "CommunityToolkit.Maui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunityToolkit.Maui.Behaviors", AssemblyName = "CommunityToolkit.Maui")]
