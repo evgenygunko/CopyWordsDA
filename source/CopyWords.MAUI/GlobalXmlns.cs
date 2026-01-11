076ede0173ed3defdfb8c5ebe8cb85e0156d5e25
Evgeny Gunko <evgeny.gunko@gmail.com>
2026-01-11 09:18:18 +0000

    Merged PR 788: Put "Faste udtryk" section into Syncfusion.Maui.Expander

--- source/CopyWords.MAUI/GlobalXmlns.cs
+++ source/CopyWords.MAUI/GlobalXmlns.cs
@@ -5,6 +5,7 @@
 
 // Syncfusion namespaces
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.Buttons", AssemblyName = "Syncfusion.Maui.Toolkit")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Expander", AssemblyName = "Syncfusion.Maui.Expander")]
 
 //[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunityToolkit.Maui.Converters", AssemblyName = "CommunityToolkit.Maui")]
